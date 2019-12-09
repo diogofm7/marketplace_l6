b242891aa8273970806c6215493d4fa87fc3b0d9
diogofm7 <33474923+diogofm7@users.noreply.github.com>
2019-12-09 12:25:44 -0300

    Seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->insert(
+/*        \DB::table('users')->insert(
             [
                 'name' => 'Administrator',
                 'email' => 'admin@admin.com.br',
@@ -19,6 +19,8 @@ class UsersTableSeeder extends Seeder
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => 'asdsauodibasobdoas',
             ]
-        );
+        );*/
+
+        factory(\App\User::class, 40)->create();
     }
 }
