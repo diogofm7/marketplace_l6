739d7a2b93a97ae44c68b647faadbc4bc262225b
diogofm7 <33474923+diogofm7@users.noreply.github.com>
2019-12-09 16:31:45 -0300

    Criação Migrations categories e category_product, e criação do Model Category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
 }
--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
 }
